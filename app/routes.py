9a1e08b896f7ad87e382d00f062e8098586f3d5b
4cos4m <anvlkostenkova@gmail.com>
2021-09-09 21:15:27 +0300

    logo ficha -try_004 + errorhandler

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,12 @@ path = Blueprint(
                         template_folder="templates"
                     )
 
+bp = Blueprint('errors', __name__)
+
+@bp.app_errorhandler(404)
+def handle_404(err):
+    return render_template('404.html'), 404
+
 @path.route('/', methods=['GET', 'POST'])
 def about():
     return render_template('about.html')
@@ -15,6 +21,13 @@ def about():
 def news():
     return render_template('news.html')
 
-@path.route('/test', methods=['GET', 'POST'])
-def test():
-    return render_template('index.html')
+# @path.route('/test', methods=['GET', 'POST'])
+# def test():
+#     return render_template('index.html')
+# @path.errorhandler(404)
+# def not_found_error(e):
+#     return render_template('404.html'), 404
+
+# @path.errorhandler(404)
+# def handle_bad_request(e):
+#     return 'bad request!', 404

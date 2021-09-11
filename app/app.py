63be554bbc79698d2a254cace8a66415622b53e3
4cos4m <anvlkostenkova@gmail.com>
2021-09-11 13:07:36 +0300

    app_errorhandler

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-# from flask_bootstrap import Bootstrap
+from flask import Flask, render_template
 # from flask_sqlalchemy import SQLAlchemy
 
 from routes import path
 # from models import db
+
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +11,5 @@ def create_app():
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///bachelorproject'
     app.register_blueprint(path)
     # db.init_app(app)
-    # bootstrap = Bootstrap(app)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -6,28 +6,14 @@ path = Blueprint(
                         template_folder="templates"
                     )
 
-bp = Blueprint('errors', __name__)
-
-@bp.app_errorhandler(404)
-def handle_404(err):
-    return render_template('404.html'), 404
-
 @path.route('/', methods=['GET', 'POST'])
 def about():
     return render_template('about.html')
-
 
 @path.route('/news', methods=['GET', 'POST'])
 def news():
     return render_template('news.html')
 
-# @path.route('/test', methods=['GET', 'POST'])
-# def test():
-#     return render_template('index.html')
-# @path.errorhandler(404)
-# def not_found_error(e):
-#     return render_template('404.html'), 404
-
-# @path.errorhandler(404)
-# def handle_bad_request(e):
-#     return 'bad request!', 404
+@path.app_errorhandler(404) # 'errorhandler' not working cause of blueprint usage, only 'app_errorhandler'
+def not_found_error(e):
+    return render_template('404.html'), 404

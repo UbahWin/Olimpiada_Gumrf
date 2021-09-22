c4eab60b156e0fb7645306e3fcf34596cbe6d04a
4cos4m <anvlkostenkova@gmail.com>
2021-09-22 15:47:43 +0300

    clean version -- w/ news

--- app/app.py
+++ app/app.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
-
 from routes import path
-# from models import db
-
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'rd'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///bachelorproject'
     app.register_blueprint(path)
-    # db.init_app(app)
 
     return app
--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 from app import app 
 
 db = SQLAlchemy()
-
 db.init_app(app)
 
 class Person(db.Model):

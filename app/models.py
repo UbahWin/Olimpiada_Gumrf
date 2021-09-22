042e945acc65357b8b376614df182cc3eefbc938
Anastasia <anvlkostenkova@gmail.com>
2021-09-22 12:51:38 +0300

    Delete models.py

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from app import app 
-
-db = SQLAlchemy()
-db.init_app(app)
-
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(30))
